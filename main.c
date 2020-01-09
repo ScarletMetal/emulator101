7f25c0c8ec5ba26b30aec51a95a6c3d7536e577a
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2020-01-09 13:55:03 +0200

    main.c

--- main.c
+++ main.c
@@ -3,5 +3,23 @@
 #include "core8080.h"
 
 void main() {
-	printf("main loaded successfully\n");
+	struct State8080 *state = make_state(200);
+	load_bin_file(state, 0, "code.bin");
+
+	state->sp = 150;
+
+	int running = 0;
+	uint16_t w = 0x403;
+	uint8_t hb = w >> 8; 
+	printf("hb = %x\n", hb);
+
+	while (!running) {
+		running = execute(state);
+		print_state(state);
+		printf("sp=%x\n", state->sp);
+		printf("mem[sp] is %x\n", state->memory[state->sp]);
+	}	
+	free(state->memory);
+	free(state);
 }
+
